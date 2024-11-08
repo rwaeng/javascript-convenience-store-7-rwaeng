f607f4707865f1c361165afad144a8f99bb87320
rwaeng <rwaeng0421@gmail.com>
2024-11-08 21:57:02 +0900

    feat(OutputView): 일부 수량에 대해 정가로 결제할지 여부에 대한 안내 기능 추가

--- __tests__/OutputView.test.js
+++ __tests__/OutputView.test.js
@@ -42,4 +42,15 @@ describe('OutputView 객체 테스트', () => {
     const log = `현재 ${productName}은(는) ${amount}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`;
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
   });
+
+  test('일부 수량에 대해 정가로 결제할지 여부에 대한 안내 메시지를 출력한다.', () => {
+    const logSpy = getLogSpy();
+    const productName = '콜라';
+    const amount = 1;
+
+    OutputView.askToPayFullPrice(productName, amount);
+
+    const log = `현재 ${productName} ${amount}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`;
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+  });
 });
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -30,6 +30,10 @@ const OutputView = {
       `현재 ${productName}은(는) ${quantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
     );
   },
+
+  askToPayFullPrice(productName, quantity) {
+    `현재 ${productName} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`;
+  },
 };
 
 export default OutputView;
