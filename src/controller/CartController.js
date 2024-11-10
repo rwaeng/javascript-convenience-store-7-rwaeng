b76533bbe23b87c55417710fc3ddccd256b378c3
rwaeng <rwaeng0421@gmail.com>
2024-11-11 01:49:39 +0900

    feat(CartController): 카트 컨트롤러 추가

---
+++ src/controller/CartController.js
@@ -0,0 +1,43 @@
+import Cart from '../domain/Cart.js';
+import Validator from '../domain/Validator.js';
+import InputView from '../View/InputView.js';
+
+const CartController = {
+  async initCart(stock) {
+    const products = await InputView.getProducts();
+
+    products.forEach(product => {
+      Validator.validateCartItem(stock.getStock(), product);
+    });
+
+    const cart = new Cart(products);
+    return cart;
+  },
+
+  async addRemoveCartItem(cart, item, quantity, count) {
+    const answer = await InputView.getYesNo();
+
+    if (answer === 'N') {
+      cart.removeCartItem(item.name, quantity);
+      cart.addPromotionItem(item.name, count);
+    }
+    if (answer === 'Y') {
+      cart.addPromotionItem(item.name, count);
+    }
+  },
+
+  async addCartItem(cart, name, quantity, promoCount) {
+    const answer = await InputView.getYesNo();
+
+    if (answer === 'N') {
+      cart.addPromotionItem(name, promoCount);
+      cart.removeCartItem(name, quantity);
+    }
+    if (answer === 'Y') {
+      cart.addCartItem(name, quantity);
+      cart.addPromotionItem(name, quantity);
+    }
+  },
+};
+
+export default CartController;
