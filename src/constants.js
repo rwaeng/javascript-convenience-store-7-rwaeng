dd325cc00c2e95e52a622282b7f5d05f064803fc
rwaeng <rwaeng0421@gmail.com>
2024-11-10 23:42:16 +0900

    feat(constants): 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,5 @@
 const REG_EXP = /^\[(.+)-(\d+)\]$/;
 
-const INPUT = Object.freeze({
-  
-});
-
 const OUTPUT = Object.freeze({
   WELCOME: '안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n',
   GET_PRODUCTS:
@@ -17,6 +13,12 @@ const ERROR = Object.freeze({
     WRONG: '[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.',
     INVALID_FORMAT:
       '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
+  },
+  STOCK: {
+    INVALID_PRODUCT: '[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.',
+    OVER_QUANTITY:
+      '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
+    UNDER_QUANTITY: '[ERROR] 유효하지 않은 수량입니다. 다시 입력해 주세요.',
   },
 });
 
