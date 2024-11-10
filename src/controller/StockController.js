275680ac2fb31ee995861965cc79800d9f5bca4c
rwaeng <rwaeng0421@gmail.com>
2024-11-11 01:49:53 +0900

    feat(StockController): 재고 컨트롤러 추가

---
+++ src/controller/StockController.js
@@ -0,0 +1,19 @@
+import Stock from '../domain/Stock.js';
+import InputView from '../View/InputView.js';
+
+const StockController = {
+  initStock() {
+    const productData = InputView.readFile('public/products.md');
+    const stock = new Stock(productData);
+
+    return stock;
+  },
+  updateStock(stock, soldItems) {
+    const removePromotion = soldItems.filter((item) => !item.promotion)
+    removePromotion.forEach(item =>
+      stock.reduceStock(item.name, item.quantity),
+    );
+  },
+};
+
+export default StockController;
