35053aed5a8762faf4708477685e97e1bca275f5
rwaeng <rwaeng0421@gmail.com>
2024-11-10 23:44:57 +0900

    feat(Validator): 카트 아이템 등록시 유효성 검사 함수 작성

--- src/domain/Validator.js
+++ src/domain/Validator.js
@@ -1,20 +1,49 @@
-import { ERROR, REG_EXP } from '../constants';
+import { ERROR, REG_EXP } from '../constants.js';
 
 const Validator = {
   validateIsNull(input) {
     if (!input) throw new Error(ERROR.INPUT.WRONG);
   },
+
   validateInputFormat(input) {
     if (!REG_EXP.test(input)) throw new Error(ERROR.INPUT.INVALID_FORMAT);
   },
+
   validateInput(input) {
     this.validateIsNull(input);
     this.validateInputFormat(input.split(','));
   },
+
   validateYesNO(input) {
     const upperCase = input.toUpperCase();
     if (upperCase !== 'Y' && upperCase !== 'N')
       throw new Error(ERROR.INPUT.WRONG);
+  },
+
+  validateExistProductName(stock, product) {
+    if (!stock.some(item => item.name === product.name)) {
+      throw new Error(ERROR.STOCK.INVALID_PRODUCT);
+    }
+  },
+
+  validateUnderQuantity(product) {
+    if (product.quantity < 0) throw new Error(ERROR.STOCK.UNDER_QUANTITY);
+  },
+
+  validateOverQuantity(stock, product) {
+    const totalQuantity = stock
+      .filter(item => item.name === product.name)
+      .reduce((sum, item) => sum + item.quantity, 0);
+
+    if (product.quantity > totalQuantity) {
+      throw new Error(ERROR.STOCK.OVER_QUANTITY);
+    }
+  },
+
+  validateCartItem(stock, product) {
+    this.validateExistProductName(stock, product);
+    this.validateUnderQuantity(product);
+    this.validateOverQuantity(stock, product);
   },
 };
 
